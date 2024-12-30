9d8638659c23872f1fdf4ba03b503232c5460fa5
bonboru93 <bonboru93@bonboru93deMacBook-Pro.local>
2024-12-30 16:32:18 +0800

    add more icon

--- src/SliderCaptcha.jsx
+++ src/SliderCaptcha.jsx
@@ -3,6 +3,11 @@ import "./SliderCaptcha.less"
 
 import defaultBgImg from "./assets/bg.jpg"
 import defaultPuzzleImg from "./assets/puzzle.png"
+import arrowRightIcon from "./assets/arrow_right.svg"
+import okIcon from "./assets/ok.svg"
+import errIcon from "./assets/err.svg"
+import loadingIcon from "./assets/loading.svg"
+
 
 const BG_WIDTH = 320
 const PUZZLE_WIDTH = 60
@@ -10,13 +15,37 @@ const HANDLER_SIZE = 40
 const PUZZLE_WALK = BG_WIDTH - PUZZLE_WIDTH
 const HANDLER_WALK = BG_WIDTH - HANDLER_SIZE
 
-const SliderCaptcha = ({ onVerify }) => {
+const iconMap = {
+    ready: arrowRightIcon,
+    checking: loadingIcon,
+    ok: okIcon,
+    err: errIcon
+}
+
+const colorMap = {
+    ready: ['#1991fa', '#d1e9fe'],
+    checking: ['#1991fa', '#d1e9fe'],
+    ok: ['#52ccba', '#d2f4ef'],
+    err: ['#f57a7a', '#fce1e1']
+}
+
+const SliderCaptcha = ({ onCheck }) => {
     const [puzzlePos, setPuzzlePos] = useState(0)
     const handlerPos = puzzlePos * HANDLER_WALK / PUZZLE_WALK
 
+    const [status, setStatus] = useState('ready')
+
     const _onVerify = v => {
-        onVerify(v)
-        setPuzzlePos(0)
+        setStatus('checking')
+        onCheck(v)
+            .then(() => setStatus('ok'))
+            .catch(() => setStatus('err'))
+            .finally(() => setTimeout(() => {
+                setPuzzlePos(0)
+                setTimeout(() => {
+                    setStatus('ready')
+                }, 0);
+            }, 500))
     }
 
     return (
@@ -30,10 +59,22 @@ const SliderCaptcha = ({ onVerify }) => {
                     onChange={e => setPuzzlePos(Number(e.target.value))}
                     onMouseUp={e => _onVerify(Number(e.target.value))}
                     onTouchEnd={e => _onVerify(Number(e.target.value))}
+                    onKeyDown={e => e.preventDefault()}
+                    style={{ pointerEvents: status === 'ready' ? 'unset' : 'none' }}
                 />
-                <div className="rail" style={{ gridTemplateColumns: `${handlerPos}px auto` }} />
+                <div className="rail" style={{ gridTemplateColumns: `${handlerPos}px auto` }}>
+                    <div style={{
+                        background: colorMap[status][1],
+                        borderColor: colorMap[status][0]
+                    }} />
+                </div>
                 <div className="hint">拖动滑块完成拼图</div>
-                <div className="handler" style={{ left: handlerPos }} />
+                <div className="handler" style={{
+                    left: handlerPos,
+                    background: colorMap[status][0]
+                }}>
+                    <img src={iconMap[status]} className={status} />
+                </div>
             </div>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import SliderCaptcha from './SliderCaptcha.jsx'
 
+
+const onCheck = () => new Promise((resolve, reject) =>
+  setTimeout(Math.random() > .5 ? resolve : reject, 500))
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <dialog open style={{ inset: 0 }}>
-      <SliderCaptcha onVerify={console.log} />
+      <SliderCaptcha onCheck={onCheck} />
     </dialog>
   </StrictMode>,
 )
